e9b6830282a3384b575ee58274f4aa74f5eaba69
Anderson Kamiya <akamiya@AKATECNOLOGIA.COM>
2022-06-07 18:34:33 -0300

    initializeMiddleware

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -3,5 +3,6 @@
 module.exports = async ({ strapi }) => {
   // bootstrap phase
   await strapi.plugin('aka-plugins').service('mainService').initializePlugin();
+  await strapi.plugin('aka-plugins').service('akaMonitor').initializeMiddleware();
   await strapi.plugin('aka-plugins').service('akaMigration').syncMigration();
 };
